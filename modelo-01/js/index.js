9fd5ced4c26c14ea9fcd1030a925b1878e0e2c22
Gaby Aguilera <gaby_ag@live.com.ar>
2023-08-04 22:34:53 -0300

    avances

---
+++ modelo-01/js/index.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+
+ sr.reveal('.frase, .dia, .hora, .lugar, .dresscode, .asistencia, .cancion, .insta, .tips, .fotos, .despedida', {
+    duration: 4000,
+    origin: 'bottom', // Animación desde abajo
+    distance: '50px' 
+});
+
+
+  
+  
+  
+  
+  
+  
+  
+  
