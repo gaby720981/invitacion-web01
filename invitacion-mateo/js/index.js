94f16567999c451550c9f264e89e60abd2ec2c18
Gaby Aguilera <gaby_ag@live.com.ar>
2024-03-21 02:00:19 -0300

    inv mateo

---
+++ invitacion-mateo/js/index.js
@@ -0,0 +1,74 @@
+window.sr = ScrollReveal();
+
+ sr.reveal('.frase, .dia, .hora, .lugar, .dresscode, .asistencia, .cancion, .insta, .tips, .fotos', {
+    duration: 1500,
+    origin: 'bottom', // Animación desde abajo
+    distance: '80px' 
+});
+sr.reveal('.despedida', {
+  duration: 5000,
+  origin: 'bottom', // Animación desde abajo
+  distance: '100px' 
+});
+
+const backgroundMusic = document.getElementById('backgroundMusic');
+const playButton = document.getElementById('playButton');
+let isMusicPlaying = false;
+
+playButton.addEventListener('click', () => {
+  if (isMusicPlaying) {
+    backgroundMusic.pause();
+  } else {
+    backgroundMusic.play();
+  }
+  isMusicPlaying = !isMusicPlaying;
+  updatePlayButtonState();
+});
+
+backgroundMusic.addEventListener('ended', () => {
+  isMusicPlaying = false;
+  updatePlayButtonState();
+});
+
+function updatePlayButtonState() {
+
+  const playIcon = playButton.querySelector('i');
+
+
+  playIcon.className = isMusicPlaying ? 'fa-solid fa-pause' : 'fa-solid fa-play';
+  playIcon.style.color = '#AD664D'; 
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('button');
+
+  buttons.forEach(button => {
+    // Verificar si el botón no tiene el id "playButton"
+    if (button.id !== 'playButton') {
+      button.addEventListener('click', function () {
+        this.classList.toggle('clicked');
+      });
+    }
+  });
+});
+
+document.addEventListener('scroll', function() {
+  var scrollTop = window.scrollY;
+  var video = document.querySelector('.mobile-video');
+  var maxOpacity = 0.009; // Ajusta el valor máximo de opacidad
+  var opacity = 1 - Math.pow(scrollTop / 100, 2) * maxOpacity; // Utiliza Math.pow para aplicar una función no lineal
+
+  video.style.opacity = Math.max(opacity, 0).toFixed(2); // Garantiza que la opacidad no sea inferior a 0
+});
+
+
+
+
+
+
+
+ 
+
+ 
