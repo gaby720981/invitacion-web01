88fe44000d2ff44dace089f81440d2d87fea87b3
Gaby Aguilera <gaby_ag@live.com.ar>
2024-03-22 19:57:54 -0300

    pruebo js

--- invitacion-patricia/js/index.js
+++ invitacion-patricia/js/index.js
@@ -68,7 +68,9 @@ document.addEventListener('scroll', function() {
 });
 
 
-
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+  document.querySelector('.container').style.backgroundAttachment = 'local';
+}
 
 
 
